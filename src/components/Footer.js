26670e6b467c4f49837fac5b5b4882e0e94ce8e8
VictorAlmeidaSiLva <victoraugustoas41@gmail.com>
2024-10-04 03:46:12 -0300

    feat: :sparkles: Começando o market
    Criando os components d market

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 w-full py-4 mt-8">
+      <p className="text-white text-center">© 2024 Carnes à Venda. Todos os direitos reservados.</p>
+      <p className="text-white text-center">Contato: contato@carnesavenda.com</p>
+    </footer>
+  );
+}
